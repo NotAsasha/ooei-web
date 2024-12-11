d2d9493c79a90b35182357754ac8362190fb51b7
NotAsahsa <sdhchdhxhshd@gmail.com>
2024-12-11 03:36:44 +0200

    Added A Bit Of Design

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-let img1 = document.getElementById('img1');
-let img2 = document.getElementById('img2');
-let img3 = document.getElementById('img3');
-let img4 = document.getElementById('img4');
+img5.style.right = 0;
 
 window.addEventListener('scroll', function() {
 
     let value = window.scrollY;
-    img4.style.marginTop = value * 0.75+'px';
-    img3.style.marginTop = value * 0.5+'px';
-    img2.style.marginTop = value * 0.25+'px';
+    img4.style.marginTop = value +'px';
+    img3.style.marginTop = value * 0.8+'px';
+    img2.style.marginTop = value * 0.6+'px';
+    img1.style.marginTop = value * 0.4+'px';
 
     img5.style.marginTop = value * 0.7+'px';
     img5.style.right = value * 0.4+'px';
 
+    svgText.style.marginTop = value + 'px';
+    svgText.style.width = 50 - value / 30 + '%';
+    svgPath.style.strokeDashoffset = value / 6;
 })
