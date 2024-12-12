52d1003f41908ce9f8f5d0c02c14a22b37ad2ed3
NotAsahsa <sdhchdhxhshd@gmail.com>
2024-12-12 04:56:16 +0200

    Fixed button covering (not really, just made it less noticible)

--- games.js
+++ games.js
@@ -3,4 +3,5 @@ window.addEventListener('scroll', function () {
 
     let value = window.scrollY;
     toDestroy.style.opacity = 100 - value + '%';
+    toDestroy.style.marginTop = -value * 2 + 'px';
 })
